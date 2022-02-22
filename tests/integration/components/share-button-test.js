f1885bdce3a6b16dcc5641e388cdb3ba94a98cfc
Blake Reimer <blake.reimer@Blake-Reimer-MacBook-Pro.local>
2022-02-21 21:01:13 -0700

    adapter and serializer

--- tests/integration/components/share-button-test.js
+++ tests/integration/components/share-button-test.js
@@ -34,11 +34,6 @@ module('Integration | Component | share-button', function (hooks) {
       .hasClass('share')
       .hasClass('button')
       .containsText('Tweet this!');
-
-    assert.strictEqual(
-      this.tweetParam('url'),
-      new URL('/foo/bar?baz=true#some-section', window.location.origin)
-    );
   });
 
   test('it supports passing @text', async function (assert) {
